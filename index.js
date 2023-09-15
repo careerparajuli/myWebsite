363251e6a1159dfbcbe6f8752b67107d0c6c51ab
careerparajuli <93539685+careerparajuli@users.noreply.github.com>
2023-09-14 18:29:56 -0600

    Updated Experience page

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const text = "Hi!, I am Ujjwal Parajuli, a full stack developer.";
+
 const textContainer = document.getElementById("typewriter-text");
 
 function typeWriter(text, i) {
-    if (i < text.length) {
-        textContainer.innerHTML += text.charAt(i);
-        const audio = new Audio("keyboard-click.mp3"); // Replace with the path to your keyboard click sound
-        audio.play();
-        setTimeout(function () {
-            typeWriter(text, i + 1);
-        }, 100); // Adjust the typing speed here (milliseconds)
-    }
+  if (i < text.length) {
+    textContainer.innerHTML += text.charAt(i);
+    const audio = new Audio("keyboard_sound.mp3");
+    audio.play();
+    setTimeout(function () {
+      typeWriter(text, i + 1);
+    }, 100); // Adjusting the typing speed here (milliseconds)
+  }
 }
 
 // Start the typing animation when the page loads
 window.onload = function () {
-    typeWriter(text, 0);
+  typeWriter(text, 0);
 };
